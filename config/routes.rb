1fee7ff1ba229a30cd37ef743913d7318791f224
Mike Salisbury <michael.salisbury87@gmail.com>
2017-08-22 06:18:21 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get '/home', to: 'static_pages#home', as: :home
+  get '/about', to: 'static_pages#about', as: :about
+  get '/help', to: 'static_pages#help', as: :help
+  get '/contact', to: 'static_pages#contact', as: :contact
+  get '/signup', to: 'users#new'
   
   
 
